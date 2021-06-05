5b2d52d4ab697746bac9f15d1034bda3c3c08ed1
Richard Anderson <80434907+Richie3173@users.noreply.github.com>
2021-06-05 23:25:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const 一番いいゲーム = ['ニーア　オートマタ', '龍が如く','鉄拳', 'つしま']
 
+一番いいゲーム.push('DMC');
+
 let randomNumber = Math.random()*一番いいゲーム.length
 
 let randomInteger = Math.floor(randomNumber)
@@ -11,4 +13,5 @@ console.log(一番いいゲーム[randomInteger])
 // math function - all together 
 
 console.log(一番いいゲーム[Math.floor(Math.random()*一番いいゲーム.length)])
+
 
